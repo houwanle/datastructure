706d9e6c85efd5b8eeefc610f802dcd3a81c86a4
houwanle <wanle@qq.com>
2020-09-26 11:03:42 +0800

    添加 构建环形链表及遍历

--- src/com/lele/linkedlist/Josephu.java
+++ src/com/lele/linkedlist/Josephu.java
@@ -10,5 +10,86 @@ package com.lele.linkedlist;
 public class Josephu {
     public static void main(String[] args) {
 
+        // 测试构建环形链表及遍历
+        CircleSingleLinkedList circleSingleLinkedList = new CircleSingleLinkedList();
+        circleSingleLinkedList.addBoy(10); // 添加小孩节点
+        circleSingleLinkedList.showBoy();
+
     }
 }
+
+// 创建一个环形的单向链表
+class CircleSingleLinkedList {
+    // 创建一个first节点，当前没有编号
+    private Boy first = null;
+
+    // 添加小孩节点，构建成一个环形链表
+    public void addBoy(int nums) {
+        // num 做一个数据校验
+        if (nums < 1) {
+            System.out.println("nums 的值不正确");
+            return;
+        }
+        Boy curBoy = null; // 辅助指针，帮助构建环形链表
+        // 使用for来创建我们的环形链表
+        for (int i = 1; i <= nums; i++) {
+            // 根据编号，创建小孩节点
+            Boy boy = new Boy(i);
+            if (i == 1) {
+                first = boy;
+                first.setNext(first); //构成环
+                curBoy = first;// 让curBoy指向第一个小孩
+            } else {
+                curBoy.setNext(boy);
+                boy.setNext(first);
+                curBoy = boy;
+            }
+        }
+    }
+
+    // 遍历当前的环形链表
+    public void showBoy(){
+        // 判断链表是否为空
+        if (first == null) {
+            System.out.println("没有任何小孩~~");
+            return;
+        }
+        // 因为first不能动，因此我们仍然使用一个辅助指针完成遍历
+        Boy curBoy = first;
+        while(true) {
+            System.out.printf("小孩的编号 %d\n", curBoy.getNo());
+            if (curBoy.getNext() == first) { // 说明已经遍历完毕
+                break;
+            }
+            curBoy = curBoy.getNext();// curBoy 后移
+        }
+    }
+}
+
+// 创建一个Boy类，表示一个节点
+class Boy {
+    private int no; // 编号
+    private Boy next; // 指向下一个节点，默认null
+
+    public Boy(int no) {
+        this.no = no;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public Boy getNext() {
+        return next;
+    }
+
+    public void setNext(Boy next) {
+        this.next = next;
+    }
+}
+
+
