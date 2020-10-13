27080167c80408d74764b25bf603f10d4308315d
houwanle <wanle@qq.com>
2020-10-14 07:27:24 +0800

    添加 希尔排序（采用移位法）

--- src/com/lele/sort/ShellSort.java
+++ src/com/lele/sort/ShellSort.java
@@ -27,11 +27,14 @@ public class ShellSort {
         String date1Str = simpleDateFormat.format(date1);
         System.out.println("排序前的时间是：" + date1Str);
 
-        shellSort(arr);
+//        shellSort(arr);
+        shellSort2(arr);
 
         Date date2 = new Date();
         String date2Str = simpleDateFormat.format(date2);
         System.out.println("排序后的时间是：" + date2Str);
+
+//        System.out.println(Arrays.toString(arr));
     }
 
     // 希尔排序时，对有序序列在插入时采用交换法，
@@ -54,7 +57,26 @@ public class ShellSort {
         }
     }
 
-
+    // 对交换式的希尔排序进行优化=>移位法
+    public static void shellSort2(int[] arr) {
+        //增量gap, 并逐步的缩小增量
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            // 从第gap个元素，逐个对其所在的组进行直接插入排序
+            for (int i = gap; i < arr.length; i++) {
+                int j = i;
+                int temp = arr[j];
+                if (arr[j] < arr[j-gap]) {
+                    while(j - gap >= 0 && temp < arr[j-gap]) {
+                        // 移动
+                        arr[j] = arr[j-gap];
+                        j -= gap;
+                    }
+                    // 当退出while后，就给temp找到插入的位置
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
 
 
 
