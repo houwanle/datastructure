1807994e28c29a6f524db5318ff6e83e17de5477
houwanle <wanle@qq.com>
2020-10-16 07:27:21 +0800

    更新 快速排序

--- src/com/lele/sort/QuickSort.java
+++ src/com/lele/sort/QuickSort.java
@@ -1,6 +1,8 @@
 package com.lele.sort;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 
 /**
  * author: hwl
@@ -12,10 +14,26 @@ import java.util.Arrays;
 public class QuickSort {
 
     public static void main(String[] args) {
-        int[] arr = {-9,78,0,23,-567,70,-1,900,4561};
+//        int[] arr = {-9,78,0,23,-567,70,-1,900,4561};
+
+        // 创建一个 有80000 个随机数的数组
+        int[] arr = new int[80000];
+        for (int i = 0; i < 80000; i++) {
+            arr[i] = (int)(Math.random()*8000000);
+        }
+
+        Date date1 = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String date1Str = simpleDateFormat.format(date1);
+        System.out.println("排序前的时间是：" + date1Str);
 
         quickSort(arr, 0, arr.length-1);
-        System.out.println(Arrays.toString(arr));
+
+        Date date2 = new Date();
+        String date2Str = simpleDateFormat.format(date2);
+        System.out.println("排序后的时间是：" + date2Str);
+
+//        System.out.println(Arrays.toString(arr));
     }
 
     public static void quickSort(int[] arr, int left, int right) {
