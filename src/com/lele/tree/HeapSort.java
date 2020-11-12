02d1492c95d726736b84c38959d05aeaa0377c78
houwanle <wanle@qq.com>
2020-11-13 07:23:52 +0800

    更新 堆排序

--- src/com/lele/tree/HeapSort.java
+++ src/com/lele/tree/HeapSort.java
@@ -13,16 +13,39 @@ public class HeapSort {
 
     public static void main(String[] args) {
         // 升序排序
-        int arr[] = {4,6,8,5,9};
+        int arr[] = {4,6,8,5,9,90,-1,0,45,100};
         heapSort(arr);
     }
 
     public static void heapSort(int arr[]) {
+        int temp = 0;
         System.out.println("堆排序！");
 
         // 分步完成
-        adjustHeap(arr, 1, arr.length);
-        System.out.println("第一次" + Arrays.toString(arr));// 4,9,8,5,6
+//        adjustHeap(arr, 1, arr.length);
+//        System.out.println("第一次" + Arrays.toString(arr));// 4,9,8,5,6
+//
+//        adjustHeap(arr, 0, arr.length);
+//        System.out.println("第二次" + Arrays.toString(arr)); //9,6,8,5,4
+
+        // 完成最终代码
+        // 将无序序列构建成一个堆，根据升序降序需求选择大顶堆或小顶堆
+        for (int i = arr.length/2 - 1; i >= 0; i--) {
+            adjustHeap(arr, i, arr.length);
+        }
+
+        /**
+         * 2.将堆顶元素与末尾元素交换，将最大元素“沉”到数组末端；
+         * 3.重新调整结构，使其满足堆定义，然后继续交换堆顶元素与当前末尾元素，反复进行调整+交换步骤，直到整个序列有序。
+         */
+        for (int j = arr.length-1; j > 0; j--) {
+            // 交换
+            temp = arr[j];
+            arr[j] = arr[0];
+            arr[0] = temp;
+            adjustHeap(arr, 0, j);
+        }
+        System.out.println("数组：" + Arrays.toString(arr));
     }
 
     /**
